3a6f90ee6a39c210d46ca304da274002a2df17b2
Zei0 <nikitos.n653@gmail.com>
2021-04-23 18:53:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,17 @@ cnv.onmousedown = (e) => {
  setTimeout(() => {
   cnv.onmousemove = (event) => {
    ctx.fillStyle = color 
-   ctx.fillRect(e.offsetX - width/2, e.offsetY - width/2, width, width)
+   ctx.fillRect(event.offsetX - width/2, event.offsetY - width/2, width, width)
    
  }
- }, )}
+ }, 5)
+
+ ctx.fillStyle = color 
+ctx.fillRect(e.offsetX - width/2, e.offsetY - width/2, width, width)
  
- cnv.onmouseup = () => {
+   cnv.onmouseup = () => {
      cnv.onmousemove = null
  }
+}
+ 
+
